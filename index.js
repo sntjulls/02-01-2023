5b26326eece1fc8d7bc643dbd94db86228086968
“Yulia <julie.kovalchuk@gmail.com>
2023-01-02 16:57:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// alert(123)
+// window.alert(456)
+
+/*console.log(123)
+console.log(12.3)
+console.log('te"xt')
+console.log(true)
+console.log('true')
+console.log(null)
+console.log(undefined)
+
+починається з букви, $ або _
+не використовувати пробіл
+регістрозалежне
+відповідає на питання, що в ній
+*/
+
+let userName = 'Brad';
+console.log(userName);
+
+let userAge; //оголошення
+console.log(typeof userAge);
+userAge = 59n; //ініціалізація
+console.log(typeof userAge);
+
+
+const userYearOfBirth = 1963;
+
+// console.log(userName);
+// console.log(userAge);
+console.log('user age:'. userAge)
+
+userAge = 60;
+userName = "Bradly";
+
+const isUserMale = true;
+// const userGender = 'male';
+// userGender = 'female'; //error
+
+const AMOUNT_DAYS_WEEK = 7;
+
+
+
+
+
+let number = 12;
+
+{
+
+    let number =  89;
+    console.log(number);
+}
+
+console.log(number);
